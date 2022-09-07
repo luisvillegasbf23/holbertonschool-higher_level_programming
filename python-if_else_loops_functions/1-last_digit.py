6c7d15b26a3a9e796836f21a34509e5e479427f2
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-06 23:41:55 -0300

    fix: task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if temp > 5:
     print( 'and is greater than 5')
 if temp == 0:
       print(' and is 0')
-if temp < 6 & temp != 0:
+if temp < 6 and temp != 0:
     print(' and is less than 6 and not 0')
 
 
