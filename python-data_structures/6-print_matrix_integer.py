2590faea6019b5d9df3abaf4aa262247b0211fff
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-12 23:08:42 -0300

    fix: task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for val in row:
-            print('{:d}'.format(val), end=" ")
-        print()
+    for i in matrix:
+        print(' '.join('{:d}'.format(j) for j in i))
