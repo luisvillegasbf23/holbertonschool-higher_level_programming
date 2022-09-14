575130732d0eb56453438df191f1f673f8b99d9e
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-14 15:04:46 -0300

    fix: task 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a = set(set_1)
-    b = set(set_2)
-    res = a & b
-    if (res):
-        return list(res)
+    return set_1.intersection(set_2)
