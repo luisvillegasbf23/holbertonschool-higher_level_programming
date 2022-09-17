724f7bac62e895bff34c9a1f5641f02f601a11e8
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-17 09:35:52 -0300

    fix: task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    j = 0
     try:
         for i in range(x):
             print('{}'.format(my_list[i]), end='')
+            j += 1
     except:
         print('', end='')
     finally:
         print('')
-    return x
+    return j
