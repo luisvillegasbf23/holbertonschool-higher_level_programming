3ba5ef752536661abe85f1b440cce959674887c3
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-24 19:57:59 -0300

    feat: task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''Write an empty class Rectangle that defines a rectangle'''
+
+
+class Rectangle:
+    ''' init  '''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            x = (self.width * 2) + (self.height * 2)
+            return x
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''setter width'''
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''setter width'''
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
