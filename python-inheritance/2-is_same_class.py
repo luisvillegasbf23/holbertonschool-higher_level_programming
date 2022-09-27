6fcbf39b108943cd7cf5700ae59e4a5f1355a4da
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-26 23:37:08 -0300

    feat: task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+''' Exact same object  '''
+
+
+def is_same_class(obj, a_class):
+    ''' function  '''
+    if not isinstance(obj, a_class):
+        return False
+    else:
+        return True
