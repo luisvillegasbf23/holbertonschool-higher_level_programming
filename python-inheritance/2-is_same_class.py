74c65676b5e6febd768da249b6ed389dfe67fa7d
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-26 23:59:19 -0300

    fix: task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,13 +4,7 @@
 
 def is_same_class(obj, a_class):
     ''' function  '''
-    if type(obj) == bool:
-        return False
-    if type(obj) is None:
-        return False
-    if type(obj) == list:
-        return False
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
