03000b6a87399897a13ff2bf70fd9fb022bd1928
luisvillegasbf23 <luisville.lv@gmail.com>
2022-09-28 21:29:24 -0300

    feat: task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Load, add, save  '''
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file('add_item.json')
+except Exception:
+    my_list = []
+
+for i in sys.argv[1:]:
+    my_list.append(i)
+
+save_to_json_file(my_list, "add_item.json")
