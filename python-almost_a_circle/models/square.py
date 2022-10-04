1403c062ecbe0baf237a4d7abeb16b411fe534c1
luisvillegasbf23 <luisville.lv@gmail.com>
2022-10-04 20:31:58 -0300

    fix: task 12 add kwargs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -36,3 +36,13 @@ class Square(Rectangle):
                 self.x = args[2]
             if len(args) > 3:
                 self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
