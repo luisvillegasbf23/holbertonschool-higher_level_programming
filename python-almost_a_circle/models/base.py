dd26cbe6438242c6d2d6c048416b2e8e7d5eba1b
luisvillegasbf23 <luisville.lv@gmail.com>
2022-10-04 22:03:35 -0300

    feat: task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,14 @@ class Base:
             return "[]"
         temp = json.dumps(list_dictionaries)
         return temp
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' save json in a file  '''
+        file = cls.__name__ + ".json"
+        temp = []
+        if list_objs:
+            for i in list_objs:
+                temp.append(i.to_dictionary())
+        with open(file, "w") as f:
+            f.write(Base.to_json_string(temp))
