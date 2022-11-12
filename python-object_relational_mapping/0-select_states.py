0bab054c260671ed535e2d9444680f3481abfa85
luisvillegasbf23 <luisville.lv@gmail.com>
2022-11-11 18:29:56 -0800

    fix: py8 documented

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,29 +3,29 @@
 
 import sys
 import MySQLdb
+if __name__ == "__main__":
+    # take args from the promp
+    arg = sys.argv
+    DB_HOST = 'localhost'
+    DB_USER = arg[1]
+    DB_PASS = arg[2]
+    DB_NAME = arg[3]
 
-# take args from the promp
-arg = sys.argv
-DB_HOST = 'localhost'
-DB_USER = arg[1]
-DB_PASS = arg[2]
-DB_NAME = arg[3]
+    data = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
 
-data = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
+    # connect to db
+    conn = MySQLdb.connect(*data)
 
-# connect to db
-conn = MySQLdb.connect(*data)
+    #create cursor
+    cursor = conn.cursor()
 
-#create cursor
-cursor = conn.cursor()
+    # exe query
+    query = 'SELECT * FROM states ORDER BY id ASC'
+    cursor.execute(query)
 
-# exe query
-query = 'SELECT * FROM states ORDER BY id ASC'
-cursor.execute(query)
+    # Fetch the results of a select
+    res = cursor.fetchall()
 
-# Fetch the results of a select
-res = cursor.fetchall()
-
-# print response
-for i in res:
-    print(i)
+    # print response
+    for i in res:
+        print(i)
