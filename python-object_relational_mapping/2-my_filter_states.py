951e9bfc6d93cf503a45a0c3d691c67a2fb6c069
luisvillegasbf23 <luisville.lv@gmail.com>
2022-11-11 19:05:52 -0800

    feat:task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    # take args from the promp
+    arg = sys.argv
+    DB_HOST = 'localhost'
+    DB_USER = arg[1]
+    DB_PASS = arg[2]
+    DB_NAME = arg[3]
+    ARG_WORD = arg[4]
+
+    data = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
+
+    # connect to db
+    conn = MySQLdb.connect(*data)
+
+    # create cursor
+    cursor = conn.cursor()
+
+    # exe query
+    query = "SELECT * FROM states WHERE name"
+    query_part_two = f"LIKE BINARY '{ARG_WORD}' ORDER BY states.id ASC"
+
+    cursor.execute(f'{query} {query_part_two}')
+
+    # Fetch the results of a select
+    res = cursor.fetchall()
+
+    # print response
+    for i in res:
+        print(i)
