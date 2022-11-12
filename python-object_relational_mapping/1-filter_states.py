8525ab3eb45e96c0f0d10ff2737c145df321c034
luisvillegasbf23 <luisville.lv@gmail.com>
2022-11-11 18:46:32 -0800

    feat: task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    # take args from the promp
+    arg = sys.argv
+    DB_HOST = 'localhost'
+    DB_USER = arg[1]
+    DB_PASS = arg[2]
+    DB_NAME = arg[3]
+
+    data = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
+
+    # connect to db
+    conn = MySQLdb.connect(*data)
+
+    # create cursor
+    cursor = conn.cursor()
+
+    # exe query
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    cursor.execute(query)
+
+    # Fetch the results of a select
+    res = cursor.fetchall()
+
+    # print response
+    for i in res:
+        print(i)
