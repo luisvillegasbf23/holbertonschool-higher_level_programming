a697c393f93ac62bbf2f88364056063b8b450a6b
luisvillegasbf23 <luisville.lv@gmail.com>
2022-11-12 15:29:56 -0800

    feat: task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Task 7 """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sys import argv
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(
+                argv[1],
+                argv[2],
+                argv[3]
+                ),
+            pool_pre_ping=True,
+            echo=False
+            )
+
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
