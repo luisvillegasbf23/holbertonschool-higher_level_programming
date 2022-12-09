cf2d6f8751acbe9a6fd0db836ebcecfc2533d226
luisvillegasbf23 <luisville.lv@gmail.com>
2022-12-09 12:06:23 -0300

    fix: task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-//#!/usr/bin/env node
+// #!/usr/bin/env node
 // prints a message depending of args
 if (process.argv.length <= 2) {
   console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
