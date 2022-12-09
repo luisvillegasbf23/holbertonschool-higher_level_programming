a5b87ff906500addf004370c779b38310a21a066
luisvillegasbf23 <luisville.lv@gmail.com>
2022-12-09 12:44:33 -0300

    fix???????????: task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // #!/usr/bin/env node
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-arr.forEach((arr)=> {
-  console.log(arr)
+arr.forEach((arr) => {
+  console.log(arr);
 })
