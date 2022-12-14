d45f4ecd8bfc1559d526df40a789b2aeb304a416
luisvillegasbf23 <luisville.lv@gmail.com>
2022-12-13 21:13:10 -0300

    feat: task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // #!/usr/bin/env node
 // File System Object
-const file = process.argv[2]
+
+const file = process.argv[2];
 const  fs = require('fs');
-
-
 function bar (err, data){
   err ? console.log(err) : console.log(data);
 };
 
 // (file, encoding, function)
-fs.readFile(file,'utf-8', bar)
+fs.readFile(file,'utf-8', bar);
